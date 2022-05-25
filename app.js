47d9b12681cdb3374190cb879a77e1a567377b00
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-05-25 10:08:28 -0500

    greeting features done

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem("username");
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    greeting.innerText = `Hello ${username}!`;
-    greeting.classList.remove("hidden");
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 
+}
+
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
